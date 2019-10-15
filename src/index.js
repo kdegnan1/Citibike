71a01e2974dc63c080868b3c63f379c2bd42e80e
kdegnan1 <katherine.degnan@gmail.com>
2019-10-15 16:30:26 -0400

    changed project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import './styles/index.scss';
 
 window.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('app').innerText = "Hello World!";
+    console.log('loaded')
+    fetch('https://cors-anywhere.herokuapp.com/http://citibikenyc.com/stations/json'
+).then(res => console.log(res)) 
 });
+
+// window.addEventListener('DOMContentLoaded', () => {
+//     console.log('loaded')
+//     fetch('https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=40.6655101,-73.89188969999998&destinations=40.6905615,-73.9976592&key=AIzaSyB0eLDJgLGyoqtqyA7cHYbexDoGjuFfxuk'
+// ).then(res => console.log(res)) 
+// });
