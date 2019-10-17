eeb2ddbf0422bd0ba3a3ebcfce3c776002ece90e
kdegnan1 <katherine.degnan@gmail.com>
2019-10-17 10:42:59 -0400

    changes

--- src/index.js
+++ src/index.js
@@ -5,19 +5,28 @@ window.addEventListener('DOMContentLoaded', () => {
     console.log('loaded')
     makeRequest('GET', 'https://cors-anywhere.herokuapp.com/http://citibikenyc.com/stations/json')
     .then((datums) => {
-    let info = []; 
     JSON.parse(datums)["stationBeanList"].forEach( bike => { 
+        
+        const contentString = `<div>${'Available Bikes:  '}</div><div>${bike['availableBikes']}</div>`
 
-        let icon = {
-            url: 'https://i.imgur.com/4dysiV6.png', 
-            scaledSize: new google.maps.Size(50, 50), // scaled size
-        };
+            let icon = {
+                url: 'https://i.imgur.com/4dysiV6.png', 
+                scaledSize: new google.maps.Size(50, 50), // scaled size
+            };
+
             let marker = new google.maps.Marker({
                 position: { lat: bike['latitude'], lng: bike['longitude'] },
-                title: 'Available Bikes:  ' +  bike['availableBikes'].toString(), 
-                icon: icon
-                // icon: 'http://maps.google.com/mapfiles/kml/paddle/blu-blank-lv.png'
+                // title: 'Available Bikes:  ' +  bike['availableBikes'].toString(), 
+                icon: icon, 
+                htmlContent: contentString
             });
+           
+
+            marker.addListener("click", function () {
+                infoWindow.setContent(marker.htmlContent)
+                infoWindow.open(map, marker);
+            });
+
         marker.setMap(map) 
         })}
     )}
